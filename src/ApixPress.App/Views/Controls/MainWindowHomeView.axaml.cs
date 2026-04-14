556218f9ea466a4165aea3842daa84430f0e514d
azrng <itzhangyunpeng@163.com>
2026-04-14 23:52:38 +0800

    refactor(ui): split main window home and overlays

---
+++ src/ApixPress.App/Views/Controls/MainWindowHomeView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+namespace ApixPress.App.Views.Controls;
+public partial class MainWindowHomeView : UserControl
+{
+    public MainWindowHomeView()
+    {
+        InitializeComponent();
+    }
+}
